83cb233feeb2fb242b5dbed5e51922c15710c48e
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-28 15:59:43 +0300

    script that isntalls node redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+import { createClient } from 'redis';
+
+
+// create redis client
+const client = createClient();
+
+// Handle connection success
+client.on('connect', () => {
+  console.log("Redis client connected to the server");
+});
+
+// Handle connection error
+client.on('error', (err) => {
+  console.log("Redis client not connected to the server: ${err.message}");
+});
